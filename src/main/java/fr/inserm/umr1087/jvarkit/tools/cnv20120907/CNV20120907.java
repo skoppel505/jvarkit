61c884ef2a843fdaf61a9d34406dce5d5e697968
plindenbaum.u915 <plindenbaum.u915@f1a8a1f2-d036-e332-b246-5030b4e38b02>
2012-09-10 15:51:15 +0000

    cont

--- src/main/java/fr/inserm/umr1087/jvarkit/tools/cnv20120907/CNV20120907.java
+++ src/main/java/fr/inserm/umr1087/jvarkit/tools/cnv20120907/CNV20120907.java
@@ -40,28 +40,56 @@ public class CNV20120907
 	private List<SAMFileReader> bams=new ArrayList<SAMFileReader>();
 	private List<BBFileReader> bbInput=new ArrayList<BBFileReader>();
 	List<QualCount> qual2count=new ArrayList<QualCount>();
+	
+	private class ReferenceBuffer
+		{
+		String name=null;
+		int start0=0;
+		byte buffer[]=null;
+
+		byte getBaseAt(String chromId,int index0)
+			{
+			if(!chromId.equals(this.name) || buffer==null || index0 < this.start0 || index0 >= (this.start0+buffer.length))
+				{
+				this.start0 =Math.max(0,this.start0- CNV20120907.this.windowSize);
+				ReferenceSequence dna= CNV20120907.this.reference.getSubsequenceAt(
+							chromId,
+							this.start0+1,//1-based
+							(this.start0+windowSize)//inclusive
+							);
+				this.buffer=dna.getBases();
+				this.name=chromId;
+				}
+			return this.buffer[index0-this.start0];
+			}
+		}
+
+	private ReferenceBuffer referenceBuffer=new ReferenceBuffer();
 
 	private CNV20120907()
 		{
 		
 		}
+
 	
-	private void run()
+
+	
+	private void run() throws Exception
 		{
 		SAMSequenceDictionary 	dict=this.reference.getSequenceDictionary();
 		for(SAMSequenceRecord chrom: dict.getSequences())
 			{
-			LOG.info("chrom:="+chrom);
+			LOG.info("chrom:="+chrom.getSequenceName()+"/"+chrom.getSequenceLength());
 			int start=0;
 			while(start+this.windowSize<= chrom.getSequenceLength())
 				{
 				Interval interval=new Interval(
-						chrom.getId(),
+						chrom.getSequenceName(),
 						start+1,
 						(start+windowSize)
 						);
 				ReferenceSequence dna=this.reference.getSubsequenceAt(
-						chrom.getId(),
+						chrom.getSequenceName(),
 						start+1,//1-based
 						(start+windowSize)//inclusive
 						);
@@ -77,18 +105,24 @@ public class CNV20120907
 						case 'g': case 'G': ++gc; break;
 						default:break;
 						}
+					if(N>0) break;
 					}
-				if(N>0) continue;
+				if(N>0)
+					{
+					++start;
+					continue;
+					}
 				
 				/** chromosome start-end*/
-				System.out.print(chrom.getId());
+				System.out.print(chrom.getSequenceName());
 				System.out.print('\t');
 				System.out.print(start);
 				System.out.print('\t');
 				System.out.print(start+windowSize);
 				
 				/** GC% */
-				System.out.print(gc/windowSize);
+				System.out.print('\t');
+				System.out.print(gc/(double)windowSize);
 				
 				/** loop over the big files */
 				for(BBFileReader bbReader: this.bbInput)
@@ -98,11 +132,11 @@ public class CNV20120907
 						double wig_total=0;
 						int wig_count=0;
 						BigWigIterator iter=bbReader.getBigWigIterator(
-								chrom.getId(),
+								chrom.getSequenceName(),
 								start,
-								chrom.getId(),
+								chrom.getSequenceName(),
 								start+windowSize,
-								true
+								false
 								);
 						while(iter.hasNext())
 							{
@@ -111,15 +145,18 @@ public class CNV20120907
 							++wig_count;
 							}
 						System.out.print('\t');
-						System.out.print(wig_total/wig_count);
+						System.out.printf("%.2f",wig_total/(double)wig_count);
 						}
 					}
+
+				
 				/** get coverage */
 				for(SAMFileReader r:bams)
 					{
 					IntervalList iL=new IntervalList(r.getFileHeader());
 					iL.add(interval);//Coordinates are 1-based closed ended. 
-					SamLocusIterator sli=new SamLocusIterator(r);
+
+					SamLocusIterator sli=new SamLocusIterator(r,iL);
 					
 					for(QualCount qc:qual2count)
 						{
@@ -129,7 +166,10 @@ public class CNV20120907
 							i.hasNext();
 							)
 						{
+						
 						SamLocusIterator.LocusInfo locusInfo=i.next();
+
+
 						for(RecordAndOffset rao:locusInfo.getRecordAndPositions())
 							{
 							for(QualCount qc:qual2count)
@@ -140,12 +180,13 @@ public class CNV20120907
 							}
 						}
 					sli.close();
+					for(QualCount qc:qual2count)
+						{
+						System.out.print('\t');
+						System.out.printf("%.2f",qc.count/(double)windowSize);
+						}
 					}
-				for(QualCount qc:qual2count)
-					{
-					System.out.print('\t');
-					System.out.print(qc.count/windowSize);
-					}
+				
 				System.out.println();
 				
 				start+=this.windowStep;
@@ -153,68 +194,96 @@ public class CNV20120907
 			}
 		}
 	
-	public static void main(String[] args)
+	private void usage()
 		{
-		CNV20120907 app=new CNV20120907();
-		try {
-			int optind=0;
-			while(optind<args.length)
+		System.err.println(
+			"Options:\n"+
+			" -f (fasta) reference fasta indexed with faidx\n"+
+			" -bw (file) add this bigwig file\n"+
+			" -q <int> add this quality treshold for BAM alignments\n"
+			);
+		}
+
+	private void run(String[] args) throws Exception
+		{
+		
+		int optind=0;
+		while(optind<args.length)
+			{
+			if(args[optind].equals("-h"))
 				{
-				if(args[optind].equals("-h"))
-					{
-					return;
-					}
-				else if(args[optind].equals("-f") && optind+1 < args.length )
-					{
-					app.reference= ReferenceSequenceFileFactory.getReferenceSequenceFile(
-							new File(args[++optind]),
-							true //cut after first whitespace
-							);
-					}
-				else if(args[optind].equals("-bw") && optind+1 < args.length )
-					{
-					BBFileReader bbReader=new BBFileReader(args[++optind]);
-					app.bbInput.add(bbReader);
-					}
-				else if(args[optind].equals("-q") && optind+1 < args.length )
-					{
-					QualCount qc=new QualCount();
-					qc.qual=Integer.parseInt(args[++optind]);
-					app.qual2count.add(qc);
-					}
-				else if(args[optind].equals("--"))
-					{
-					optind++;
-					break;
-					}
-				else if(args[optind].startsWith("-"))
-					{
-					System.err.println("Unnown option: "+args[optind]);
-					return;
-					}
-				else
-					{
-					break;
-					}
-				++optind;
-				}
-			if(optind==args.length)
-				{
-				System.err.println("No bam defined");
+				usage();
 				return;
 				}
-			while(optind!=args.length)
+			else if(args[optind].equals("-f") && optind+1 < args.length )
 				{
-				File f=new File(args[optind++]);
-				LOG.info("opening "+f);
-				SAMFileReader sfr=new SAMFileReader(f);
-				app.bams.add(sfr);
+				this.reference= ReferenceSequenceFileFactory.getReferenceSequenceFile(
+						new File(args[++optind]),
+						true //cut after first whitespace
+						);
 				}
-			app.run();
-			} 
-	catch (Exception e)
-		{
-		e.printStackTrace();
+			else if(args[optind].equals("-bw") && optind+1 < args.length )
+				{
+				BBFileReader bbReader=new BBFileReader(args[++optind]);
+				this.bbInput.add(bbReader);
+				}
+			else if(args[optind].equals("-q") && optind+1 < args.length )
+				{
+				QualCount qc=new QualCount();
+				qc.qual=Integer.parseInt(args[++optind]);
+				this.qual2count.add(qc);
+				}
+			else if(args[optind].equals("--"))
+				{
+				optind++;
+				break;
+				}
+			else if(args[optind].startsWith("-"))
+				{
+				System.err.println("Unnown option: "+args[optind]);
+				return;
+				}
+			else
+				{
+				break;
+				}
+			++optind;
+			}
+		if(this.qual2count.isEmpty())
+			{
+			QualCount qc=new QualCount();
+			qc.qual=0;
+			this.qual2count.add(qc);
+			}
+		if(this.reference==null)
+			{
+			System.err.println("Reference missing");
+			return;
+			}
+		if(optind==args.length)
+			{
+			System.err.println("No bam defined");
+			return;
+			}
+		while(optind!=args.length)
+			{
+			File f=new File(args[optind++]);
+			LOG.info("opening "+f);
+			SAMFileReader sfr=new SAMFileReader(f);
+			this.bams.add(sfr);
+			}
+		this.run();
 		}
-	}
-	}
+
+	public static void main(String[] args)
+		{
+		try
+			{
+			new CNV20120907().run(args);
+			}
+		catch (Exception e)
+			{
+			e.printStackTrace();
+			}
+		}
+}
